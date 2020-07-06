5708309ceb552bac14fa5a08ccdd64b0999678bc
chen weijian <wjchen@astri.org>
2020-07-06 11:57:20 +0800

    update comments

--- pyHowling/howling_detection.py
+++ pyHowling/howling_detection.py
@@ -9,6 +9,19 @@ from __future__ import division
 import numpy as np
 
 def papr(frame, threshold):
+    """Peak-to-Avarage Power Ratio (PAPR)
+    Returns all frequency indices where power is greater than avarage power + threshold,
+    which are possible candidates where howling occurs.
+
+    Args:
+        frame: Spectrum of one frame.
+        threshold: Power threshold value in dB.
+         The returned candidates should have power greater than average power + threshold.
+
+    Returns:
+        A list of selected frequncy indices.
+        A list of PAPR value for every freqency bin of this frame.
+    """
     power = np.abs(frame)**2
     papr = np.zeros(len(power))
     avarage = np.mean(power)
@@ -20,6 +33,17 @@ def papr(frame, threshold):
     return ret, papr
 
 def ptpr(frame, threshold):
+    """Peak-to-Threshold Power Ratio (PTPR)
+    Returns all frequency indices where power is greater than threshold,
+    which are possible candidates where howling occurs.
+
+    Args:
+        frame: Spectrum of one frame.
+        threshold: Power threshold value in dB.
+
+    Returns:
+        A list of selected frequncy indices.
+    """
     power =  np.abs(frame)**2
     ret = []
     for i in range(len(frame)):
@@ -28,8 +52,26 @@ def ptpr(frame, threshold):
     return ret
 
 #def phpr(frame, threshold):
+"""Peak-to-Harmonic Power Ratio (PHPR)
+       To-Do
+    Args:
+
+    Returns:
+
+"""
 
 def pnpr(frame, threshold):
+    """Peak-to-Neighboring Power Ratio (PNPR)
+    Returns all frequency indices of power peaks,
+    which are greater than neighboring frequency bins by a threshold.
+
+    Args:
+        frame: Spectrum of one frame.
+        threshold: Power threshold value in dB.
+
+    Returns:
+        A list of selected frequncy indices.
+    """
     power =  frame**2
     ret = []
     for i in range(5, len(frame)-5):
@@ -41,6 +83,18 @@ def pnpr(frame, threshold):
     return ret
 
 def ipmp(candidates, index):
+    """Inerframe Peak Magnitude Persistence (IPMP)
+    Temporal howling detection criteria. Candidate should meet criteria 
+    in more than 3 frames out of 5 continuous frames.
+
+    Args:
+        candidates: nFreqs X nFrames
+                    candidates[f][t] = 1 means a candidate at frequency[f]  at frame[t]
+        index: Current frame index.
+
+    Returns:
+        A list of selected frequncy indices.
+    """
     accu = np.zeros(candidates.shape[0])
 
     if(index == 2):
@@ -58,6 +112,17 @@ def ipmp(candidates, index):
 #def imsd(frame, threshold):
 
 def screening(frame, candidates):
+    """
+    Screen the candidates. Only one frequency in neighboring several frequencies is needed
+
+    Args:
+        frame: Current frame spectrum.
+        candidates: nFreqs X nFrames
+                    candidates[f][t] = 1 means a candidate at frequency[f]  at frame[t]
+
+    Returns:
+        A list of selected frequncy indices.
+    """
     ret = []
     for c in candidates:
         if len(ret)==0:
